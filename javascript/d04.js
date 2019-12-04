37af0d3fca1c626d71915737e8b3e8777a9c3167
Maxime Pigeon <mpigeon@student.42.fr>
2019-12-04 22:17:29 +0100

    d04 part 2

--- javascript/d04.js
+++ javascript/d04.js
@@ -10,11 +10,16 @@ for (let index = input[0]; index < input[1] + 1; index++) {
         }
     }
     if (isOkay) {
+        let doubles = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         for (let j = 0; j < digits.length - 1; j++) {
             if (digits[j] === digits[j+1]) {
-                response ++;
-                break;
+                doubles[digits[j]]++;
             }
+        }
+        console.log(doubles, digits);
+        if (doubles.includes(1)) {
+            console.log('++');
+            response ++;
         }
     }
 }
