69ed41238929572439a639d27353d9ab6ee2385b
Maxime Pigeon <mpigeon@student.42.fr>
2019-12-08 15:11:13 +0100

    aoc 2015 d01

---
+++ 2015/javascript/d01.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let text = fs.readFileSync("./inputs/d01.txt", "utf-8");
+let arr = text.split('');
+let firstTime = true;
+let floor = arr.reduce((memo, dir, index) => {
+    if (dir === '(') {
+        memo++;
+    } else {
+        memo--;
+    }
+    if (memo === -1 && firstTime) {
+        console.log('answer 2:',index + 1);
+        firstTime = false;
+    }
+    return memo;
+}, 0);
+console.log('answer 1:',floor);
