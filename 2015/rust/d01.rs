b5898583fe515fa731d4d9655db677697ca0a564
Maxime Pigeon <mpigeon@student.42.fr>
2020-03-15 23:58:39 +0100

    2015 d01 rust

---
+++ 2015/rust/d01.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    let mut total = 0;
+    let mut first = true;
+    let contents = fs::read_to_string("../inputs/d01.txt")
+        .expect("Something went wrong reading the file");
+    for (index, char) in contents.chars().enumerate() {
+        if char == '(' {
+            total = total + 1;
+        } else {
+            total = total - 1;
+        }
+        if total == -1 && first {
+            first = false;
+            print!("premiere fois au sous-sol\n{}\n", index + 1);
+        }
+    }
+    print!("dernier etage atteint\n{}\n", total);
+}
